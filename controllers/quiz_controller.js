7e21ae95876a7ccab3d25ab0479b57c52a0a026c
Regino Cabrera Quesada <reginocq@yahoo.es>
2015-07-12 19:09:11 +0000

    Filtro de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -13,13 +13,21 @@ exports.load = function(req, res, next, quizId) {
   ).catch(function(error) { next(error);});
 };
 
+
 // GET /quizes
-exports.index = function(req, res) {
-  models.Quiz.findAll().then(
-    function(quizes) {
-      res.render('quizes/index', { quizes: quizes});
-    }
-  ).catch(function(error) { next(error);})
+exports.index = function(req,res){
+ 	if(req.query.search) {
+		var filtro=(req.query.search||'').replace(' ', '%');
+ 		models.Quiz.findAll({where:['pregunta like ?','%'+filtro+'%'],order:'pregunta ASC'
+ 			}).then(function(quizes){
+				res.render('quizes/index', {quizes:quizes});
+			}).catch(function(error){next(error);});
+	} 
+	else {
+		models.Quiz.findAll().then(function(quizes){
+			res.render('quizes/index', {quizes:quizes});
+		}).catch(function(error) {next(error);});
+	}
 };
 
 // GET /quizes/:id
